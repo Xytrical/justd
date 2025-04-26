3ce31a84675d829c208491281a1ced7474a73689
Diqi Al-Haqqi <dq.alhaqqi@gmail.com>
2025-04-26 21:01:06 +0700

    feat: new multiple-select

--- components/docs/anatomies/multiple-select-anatomy.tsx
+++ components/docs/anatomies/multiple-select-anatomy.tsx
@@ -1,21 +1,17 @@
-import { MultipleSelect, type SelectedKey } from "@/components/ui/multiple-select"
-import { useListData } from "react-stately"
+import { MultipleSelect } from "@/components/ui/multiple-select"
 
 export default function MultipleSelectAnatomy() {
-  const selectedItems = useListData<SelectedKey>({ initialItems: [] })
   return (
     <MultipleSelect
       className="max-w-xs"
       label="Fruits"
-      selectedItems={selectedItems}
       items={[
         { id: 1, name: "Apple" },
         { id: 2, name: "Banana" },
       ]}
-      tag={(item) => <MultipleSelect.Tag textValue={item.name}>{item.name}</MultipleSelect.Tag>}
     >
       {(item) => {
-        return <MultipleSelect.Option textValue={item.name}>{item.name}</MultipleSelect.Option>
+        return <MultipleSelect.Item textValue={item.name}>{item.name}</MultipleSelect.Item>
       }}
     </MultipleSelect>
   )
--- components/docs/pickers/multiple-select/multiple-select-intent-demo.tsx
+++ components/docs/pickers/multiple-select/multiple-select-intent-demo.tsx
@@ -1,30 +1,18 @@
 "use client"
 
 import { MultipleSelect } from "@/components/ui/multiple-select"
-import type { SelectedKey } from "@/components/ui/multiple-select"
-import { useListData } from "react-stately"
 
 export default function MultipleSelectIntentDemo() {
-  const selectedItems = useListData<SelectedKey>({
-    initialItems: [fruits[0]!],
-  })
   return (
-    <MultipleSelect
-      className="max-w-xs"
-      intent="secondary"
-      label="Fruits"
-      selectedItems={selectedItems}
-      items={fruits}
-      tag={(item) => <MultipleSelect.Tag textValue={item.name}>{item.name}</MultipleSelect.Tag>}
-    >
+    <MultipleSelect className="max-w-xs" intent="secondary" label="Fruits" items={fruits}>
       {(item) => {
-        return <MultipleSelect.Option textValue={item.name}>{item.name}</MultipleSelect.Option>
+        return <MultipleSelect.Item textValue={item.name}>{item.name}</MultipleSelect.Item>
       }}
     </MultipleSelect>
   )
 }
 
-const fruits: SelectedKey[] = [
+const fruits = [
   { id: 1, name: "Apple" },
   { id: 2, name: "Banana" },
   { id: 3, name: "Cherry" },
