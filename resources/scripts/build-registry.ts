5ac9aa8dfbaabe5b83ef002ee39280e3b5175350
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-05-03 20:18:54 +0700

    fix build:registry

--- resources/scripts/build-registry.ts
+++ resources/scripts/build-registry.ts
@@ -2,7 +2,7 @@ import fs from "node:fs"
 import path from "node:path"
 
 const registryUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+  ? 'https://intentui.com'
   : "http://localhost:3000"
 
 type RegistryType =
