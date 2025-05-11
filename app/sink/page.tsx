15f53abb5f0419b60cc6c596ef0e2f1b563aac52
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-05-11 22:09:04 +0700

    fix(clean sink): clean sink

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,11 +1,3 @@
-import TableDemo from "@/components/docs/collections/table/table-demo"
-
 export default function Page() {
-  return (
-    <div className="p-32">
-      {/*<Card className='py-0'>*/}
-      <TableDemo />
-      {/*</Card>*/}
-    </div>
-  )
+  return <div />
 }
